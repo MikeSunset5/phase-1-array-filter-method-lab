39b55ccfa1e7ddb2390d8e02fcc741bc083f48a2
Michael Waldron <mikewaldron5@gmail.com>
2022-01-13 15:50:15 -0500

    commit tree 4bae22a336e9cf8ab0a4b95145aab80031654644

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+ 
+  function findMatching(array, name){
+      return array.filter(
+          possibleMatch => possibleMatch.toLowercase() === name.toLowerCase()
+      );
+
+  }
+
+  console.log(findMatching);
